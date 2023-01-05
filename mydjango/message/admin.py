0b94f2c1db34ab42988fb5f56dd3710a74097925
czvx4261 <102032551+czvx4261@users.noreply.github.com>
2023-01-05 19:18:31 +0800

    Delete mydjango directory

--- mydjango/message/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import Message # 對應 models
-
-admin.site.register(Message)
--- mydjango/performance/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import Sales # 對應 models
-
-admin.site.register(Sales)
